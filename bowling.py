07fa5c4b66062d500959c206d4f17042448d9b9a
somaSzolyak <soma.szolyak@gmail.com>
2017-07-25 18:18:25 +0200

    driedthe result calculating in cases of strikes and spares

--- bowling.py
+++ bowling.py
@@ -1,22 +1,30 @@
+def is_strike(char):
+    if char == 'X' or char == 'x':
+        return True
+    return False
+
+
+def is_spare(char):
+    if char == '/':
+        return True
+    return False
+
+
 def score(game):
     result = 0
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
+        if is_spare(game[i]):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if is_spare(game[i]) or is_strike(game[i]):
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
+                if is_strike(game[i]) and is_spare(game[i+2]):
                     result += 10 - get_value(game[i+1])
-                else:
+                elif is_strike(game[i]):
                     result += get_value(game[i+2])
         last = get_value(game[i])
         if not in_first_half:
@@ -25,7 +33,7 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if is_strike(game[i]):
             in_first_half = True
             frame += 1
     return result
@@ -36,7 +44,7 @@ def get_value(char):
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    elif is_strike(char) or is_spare(char):
         return 10
     elif char == '-':
         return 0
